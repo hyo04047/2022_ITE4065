57e16a44d54ee65b3ca9dd3adaa2a9c5d8d0c557
하태성 <2016025041@hanyang.ac.kr>
2022-09-28 02:44:10 +0900

    delete useless and add comment

--- project1/main.cpp
+++ project1/main.cpp
@@ -8,56 +8,51 @@ using namespace std;
 #define NUM_THREAD 20
 //---------------------------------------------------------------------------
 bool isstop = false;
-bool isbatchend = false;
+Joiner joiner;
+// count for working thread
 uint64_t cnt_for_thread = 0;
+// array for store result of threads' computation
 string* volatile thread_ret[NUM_THREAD];
+// array for store input query
 string inputline[NUM_THREAD];
-// vector<thread> threads;
-// vector<shared_future<void>> thread_ret(NUM_THREAD, NULL);
-// mutex mutexes[NUM_THREAD];
-// condition_variable cv[NUM_THREAD];
+// mutex and condition_variable for each thread
 pthread_t threads[NUM_THREAD];
 pthread_mutex_t mutexes[NUM_THREAD+1];
 pthread_cond_t cv[NUM_THREAD+1];
-Joiner joiner;
 //---------------------------------------------------------------------------
 void *thread_join(void* arg) {
    long tid = (long)arg;
 
-   // thread_ret[tid].clear();
+   // initialize result
    thread_ret[tid] = (string *)-1;
+   // wait for query input
    pthread_mutex_lock(&mutexes[tid]);
    pthread_cond_wait(&cv[tid], &mutexes[tid]);
    pthread_mutex_unlock(&mutexes[tid]);
-   // unique_lock<mutex> lock(mutexes[tid]);
-   // cv[tid].wait(lock);
 
    while (!isstop) {
       QueryInfo *i = new QueryInfo();
+      // get input and parse
       i->parseQuery(inputline[tid]);
+      // join at thread
       string *result = new string(joiner.join(*i));
-
+      // save result
       thread_ret[tid] = move(result);
+      // if this thread is last thread,
+      // wakeup main thread
       if (tid == (NUM_THREAD-1)) {
          pthread_mutex_lock(&mutexes[NUM_THREAD]);
          pthread_cond_signal(&cv[NUM_THREAD]);
          pthread_mutex_unlock(&mutexes[NUM_THREAD]);
       }
-      // if (tid == (cnt_for_thread-1) && isbatchend) {
-      //    pthread_mutex_lock(&mutexes[NUM_THREAD]);
-      //    isbatchend = false;
-      //    pthread_cond_signal(&cv[NUM_THREAD]);
-      //    pthread_mutex_unlock(&mutexes[NUM_THREAD]);
-      // }
+      // wait for next input
       pthread_mutex_lock(&mutexes[tid]);
-      // free(thread_arg[tid]);
       pthread_cond_wait(&cv[tid], &mutexes[tid]);
       pthread_mutex_unlock(&mutexes[tid]);
-      // cv[tid].wait(lock);
-      // lock.unlock();
    }
 
    pthread_exit(NULL);
+   // return NULL;
 }
 //---------------------------------------------------------------------------
 int main(int argc, char *argv[]) {
@@ -70,90 +65,62 @@ int main(int argc, char *argv[]) {
 
    // Preparation phase (not timed)
    // Build histograms, indexes,...
-   // threads.reserve(NUM_THREAD);
 
    for (long idx=0; idx<NUM_THREAD; ++idx) {
+      // Initialize mutexes and condition variables
       mutexes[idx] = PTHREAD_MUTEX_INITIALIZER;
       cv[idx] = PTHREAD_COND_INITIALIZER;
-
+      // Create threads
       if (pthread_create(&threads[idx], 0, thread_join, (void*)idx) < 0) {
          printf("pthread_create error\n");
          return 0;
       }
-      // auto ret = async(launch::async, thread_join, thread_cnt);
-      // threads.emplace_back(thread(&thread_join, idx));
-      // thread_ret[idx] = (string *)-1;
-      // while (thread_ret[idx] != (string *)-1) {
-      //    this_thread::yield();
-      // }
    }
+   // Mutex and condition variable for main thread
    mutexes[NUM_THREAD] = PTHREAD_MUTEX_INITIALIZER;
    cv[NUM_THREAD] = PTHREAD_COND_INITIALIZER;
    
    while (getline(cin, line)) {
       if (line == "F") {
-         // isbatchend = true;
-         // pthread_mutex_lock(&mutexes[NUM_THREAD]);
-         // isbatchend = true;
-         // pthread_cond_wait(&cv[NUM_THREAD], &mutexes[NUM_THREAD]);
-         // pthread_mutex_unlock(&mutexes[NUM_THREAD]);
+         // Flush results
          for (long idx=0; idx<cnt_for_thread; ++idx) {
-            // while (thread_ret[idx].empty()) ;
+            // Wait until threads finish
             while (thread_ret[idx] == (string *)-1) 
                pthread_yield();
             cout << *(thread_ret[idx]);
+            // Reset result for next input
             thread_ret[idx] = (string *)-1;
-            // free(thread_ret[idx]);
          }
          cnt_for_thread = 0;
          continue; // End of a batch
       }
-
+      // All thread received input and now working
       if (cnt_for_thread == NUM_THREAD) {
+         // Wait for all thread finish
          pthread_mutex_lock(&mutexes[NUM_THREAD]);
          pthread_cond_wait(&cv[NUM_THREAD], &mutexes[NUM_THREAD]);
          pthread_mutex_unlock(&mutexes[NUM_THREAD]);
+         // Flush results
          for (long idx=0; idx<cnt_for_thread; ++idx) {
-            // while (thread_ret[idx] == (string *)-1) 
-            //    pthread_yield();
             cout << *(thread_ret[idx]);
             thread_ret[idx] = (string *)-1;
-            // free(thread_ret[idx]);
          }
          cnt_for_thread = 0;
       }
       inputline[cnt_for_thread] = line;
-
+      // Wakeup worker thread with input given
       pthread_mutex_lock(&mutexes[cnt_for_thread]);
       pthread_cond_signal(&cv[cnt_for_thread]);
       pthread_mutex_unlock(&mutexes[cnt_for_thread]);
-
-      // QueryInfo *i = new QueryInfo();
-      // i->parseQuery(line);
-
-      // thread_arg[cnt_for_thread] = move(i);
-      // thread_ret[cnt_for_thread] = (string *)-1;
-
-      // unique_lock<mutex> lock(mutexes[cnt_for_thread]);
-      // cv[cnt_for_thread].notify_one();
-      // lock.unlock();
-
-      // pthread_mutex_lock(&mutexes[cnt_for_thread]);
-      // pthread_cond_signal(&cv[cnt_for_thread]);
-      // pthread_mutex_unlock(&mutexes[cnt_for_thread]);
       cnt_for_thread++;
    }
-
+   // Set flag and Wakeup all thread and Join
    isstop = true;
    for (long idx=0; idx<NUM_THREAD; ++idx) {
       pthread_mutex_lock(&mutexes[idx]);
       pthread_cond_signal(&cv[idx]);
       pthread_mutex_unlock(&mutexes[idx]);
       pthread_join(threads[idx], NULL);
-      // unique_lock<mutex> lock(mutexes[idx]);
-      // cv[idx].notify_one();
-      // lock.unlock();
-      // threads[idx].join();
    }
 
    return 0;
